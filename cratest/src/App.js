000e74739060db65f12fc139d34f739a96583f3b
Alex <roat_alexandru@yahoo.com>
2022-06-18 11:36:44 +0300

    filter

--- cratest/src/App.js
+++ cratest/src/App.js
@@ -1,7 +1,39 @@
 
 function App() {
+
+  const data = [
+    {
+      nume: 'Alex',
+      prenume: 'Mircea',
+      age: 21,
+    },
+    {
+      nume: 'Sonia',
+      prenume: 'Sanja',
+      age: 33,
+    },
+    {
+      nume: 'Manson',
+      prenume: 'George',
+      age: 12,
+    },
+    {
+      nume: 'Dorin',
+      prenume: 'Zinga',
+      age: 41,
+    }
+  ];
+
+  const handleFilter = (e) => {
+
+    const filtered = data.filter((item) => item.nume === e.target.value)
+    console.log(filtered)
+  }
+
   return (
-    <div>test</div>
+    <div>
+      <input type= 'text' placeholder='pula' onChange={handleFilter}/>
+    </div>
   );
 }
 
